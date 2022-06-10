aa40c4b485a86f19943e3ae6231a6b49b46eb2a8
KauanR <kau.matosr@gmail.com>
2022-06-10 15:03:55 -0300

    feat: tables relations

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,24 @@
 const Sequelize = require('sequelize')
 const dbConfig = require('../config/database')
 
+const Patient = require('../models/Patient')
+const Professional = require('../models/Professional')
+const Attendance = require('../models/Attendance')
+const Appointment = require('../models/Appointment')
+const Report = require('../models/Report')
+
 const connection = new Sequelize(dbConfig)
+
+Patient.init(connection)
+Professional.init(connection)
+Attendance.init(connection)
+Appointment.init(connection)
+Report.init(connection)
+
+Patient.associate(connection.models)
+Professional.associate(connection.models)
+Attendance.associate(connection.models)
+Appointment.associate(connection.models)
+Report.associate(connection.models)
 
 module.exports = connection
